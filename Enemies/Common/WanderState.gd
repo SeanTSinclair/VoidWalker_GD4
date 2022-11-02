38b540ebcab976be5347eed841092e7240bcf423
Sean Sinclair <sean.tollefsen.sinclair@gmail.com>
2022-11-02 12:06:26 +0100

    Fixed a few wander state bugs and made the enemy sprite flip based on movement direction

--- Enemies/Common/WanderState.gd
+++ Enemies/Common/WanderState.gd
@@ -1,20 +1,28 @@
 extends State
 
+@export var min_wait_time = 0.7
+@export var max_wait_time = 2.0
+
 @onready var wander_targets : Array[Vector2] = []
 
 var target = null
-var stopping_distance = 5
+var stopping_distance = 35
 
 func _ready():
 	for child in get_children():
-		wander_targets.append(child.global_position)
-		child.queue_free()
+		if child is Node2D:
+			wander_targets.append(child.global_position)
+			child.queue_free()
 
 func enter_state(actor):
 	super.enter_state(actor)
 	actor.set_animation_state("run")
 	wander_targets.shuffle()
 	target = wander_targets[0]
+	if actor.direction_to(target).x > 0:
+		actor.set_facing_direction(Vector2.RIGHT)
+	else:
+		actor.set_facing_direction(Vector2.LEFT)
 	
 func exit_state():
 	super.exit_state()
@@ -26,7 +34,8 @@ func state_logic(delta):
 	
 func check_transitions():
 	super.check_transitions()
-	if actor.position.distance_to(target) <= stopping_distance:
+	
+	if actor.global_position.distance_to(target) <= stopping_distance:
 		if state_machine.is_wandering:
 			set_state(state_machine.states.wander)
 		else:
--- Enemies/Dark Bot/DarkBot.gd
+++ Enemies/Dark Bot/DarkBot.gd
@@ -1,16 +1,28 @@
 extends CharacterBody2D
 
+@onready var sprite = $Sprite2D
 @onready var stats = preload("res://Enemies/Dark Bot/DarkBotStats.tres")
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree["parameters/playback"]
 @onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var start_position : Vector2 = position
 
+var facing_direction = Vector2.RIGHT
+
 func _physics_process(delta):
 	move_and_slide()
 
 func apply_gravity(delta):
 	velocity.y += gravity * delta
+	
+func set_facing_direction(direction):
+	if direction == Vector2.LEFT:
+		sprite.flip_h = true
+		sprite.offset = Vector2(-53, 0)
+	elif direction == Vector2.RIGHT:
+		sprite.flip_h = false
+		sprite.offset = Vector2(0, 0)
+	facing_direction = direction
 	
 func direction_to(target) -> Vector2:
 	return (target - global_position).normalized()
