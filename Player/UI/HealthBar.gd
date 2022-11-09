326036d628171952cd5b51d665bf729fbf86422e
Sean Sinclair <sean.tollefsen.sinclair@gmail.com>
2022-11-09 22:44:04 +0100

    Fine tuned some animations for the player and changed the transition mode to be controlled from the state changes instead of partially state changes and partially animation controller.

--- Player/UI/HealthBar.gd
+++ Player/UI/HealthBar.gd
@@ -7,6 +7,9 @@ extends HBoxContainer
 
 @onready var hp_icon : PackedScene = preload("res://Player/UI/HPIcon.tscn")
 
+func _ready():
+	display_health(3, 3)
+
 func display_health(current_health, max_health):
 	clear_current_ui()
 	var current_icon = 0
@@ -15,15 +18,15 @@ func display_health(current_health, max_health):
 		if current_health / max_health <= 0.4:
 			current_texture = hp_icons[1]
 		
-		if current_icon > current_health:
+		if current_icon > current_health-1:
 			current_texture = hp_icons[0]
-		elif current_icon < max_health:
+		elif current_icon < max_health-1:
 			current_texture = hp_icons[2]
 		var icon = hp_icon.instantiate()
 		icon.texture = current_texture
 		add_child(icon)
 		current_icon += 1
-
+#
 func clear_current_ui():
 	for child in get_children():
 		child.queue_free()
